575041db7e17bebb03f0c9508733dc84789b337b
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-06-12 11:16:08 +0700

    - Implement switch menu for queue operations

--- queues.cpp
+++ queues.cpp
@@ -119,6 +119,24 @@ int main() {
             cin >> ch;
             cout << endl;
 
+            switch (ch) {
+                case '1': {
+
+                    q.insert();
+                    break;
+                }
+                case '2': {
+                    q.remove();
+                    break;
+                }
+                case '3': {
+                    q.display();
+                    break;
+                }
+                case '4': {
+                    return 0;
+                }
+            }
         }
     }
 }
